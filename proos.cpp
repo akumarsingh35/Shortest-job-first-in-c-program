1450e1f97f9714cd2678c12050aafb87226bfda6
akumarsingh35 <48875479+akumarsingh35@users.noreply.github.com>
2019-04-25 01:14:08 +0530

    Update proos.cpp

--- proos.cpp
+++ proos.cpp
@@ -4,14 +4,17 @@ int main()
 	int n; int q=0,c=0,Ct=0;
     printf("Enter number of process:");
     scanf("%d",&n);
-    int bt[n],at[n],p[n],wt[n],tat[n],ct[n],rt[n],i,j,total=0,pos,temp;
+    int bt[n],at[n],p[n],wt[n],tat[n],ct[n],rt[n],i,j,total=0,pos,temp,as1;
      int a=0,b=0,b1;
  
     printf("\nEnter Burst Time and arrival time respectively\n");
     for(i=0;i<n;i++)
     {
-        printf("p%d:",i+1);
-        scanf("%d %d",&bt[i],&at[i]);
+        printf("p%d:\n",i+1);
+        printf("Enter arrival time:");
+        scanf("%d",&at[i]);
+		printf("Enter burst time:");
+		scanf("%d",&bt[i]);
        // scanf("%d",&at[i]);
         p[i]=i+1;           //contains process number
     }
@@ -38,8 +41,10 @@ int main()
                 }
             }
         }
-    for(i=0;i<n;i++)
+        for(i=0;i<n;i++)
 	{
+		if(as1 == i) i++;
+		
 		if(at[0]!=0&&c==0)
 		{
 			Ct=Ct+at[0];
@@ -50,19 +55,26 @@ int main()
 		{
 			if(bt[i]<=bt[i+1])
 			{
+			as:
 				Ct=bt[i]+Ct;
 				ct[i]=Ct;
+			} else {
+				Ct =  Ct + bt[i+1];
+				ct[i+1]=Ct;
+				as1 = i + 1;
+				goto as;
+				
 			}
 		}
-		else if(at[n]<=ct[i])
+		else if(at[n-1]<=Ct)
 		{
 			q=i;
 			goto sortbt;
 		}
-		else if(at[i]>bt[i-1])
+		/*else if(at[i]>bt[i-1])
 		{
 			Ct= Ct+at[i];
-		}
+		}*/
 		else
 		{
 			Ct=Ct+bt[i];
@@ -72,22 +84,22 @@ int main()
     sortbt:
     	{
 	
-			for(int o=q;o<n;o++)
+			for(int o=q;o<n-1;o++)
 			{
-				for(int o1 = o + 1; o1 < n; ++o1)
+				for(int o1 = q; o1 < n-o-1; o1++)
 			if (bt[o]>bt[o1])
 			 {
 				int t;
-				t=bt[o];
-				bt[o]=bt[o1];
-				bt[o1]=t;
+				t=bt[o1];
+				bt[o1]=bt[o1+1];
+				bt[o1+1]=t;
 				int t2;
-				t2=at[o];
-				at[o]=at[o1];
-				at[o1]=t2;
+				t2=at[o1];
+				at[o1]=at[o1+1];
+				at[o1+1]=t2;
 				int t3;
-				t3=p[o];
-				p[o]=p[o1];
+				t3=p[o1];
+				p[o1]=p[o1+1];
 				p[o1]=t3;
 			 }
 		    }
@@ -96,14 +108,15 @@ int main()
 		    Ct=Ct+bt[j];
 			ct[j]=Ct;}
 }
-     //for(i=0;i<n;i++)
-     //{
-     //	tat[i]=ct[i]-at[i];
-	 //}
+     for(i=0;i<n;i++)
+     {
+     	tat[i]=ct[i]-at[i];
+     	wt[i]=tat[i]-bt[i];
+	 }
 	 printf("\nProcessNo\tA.T\tB.Time\tC.T\tT.A.T\tW.T");
 	 for(int l=0;l<n;l++)
 	 {
-	 	printf("\n%d\t\t%d\t%d\t%d\t%d\t%d",p[l],at[l],bt[l],ct[l],tat[i],wt[i]);
+	 	printf("\n%d\t\t%d\t%d\t%d\t%d\t%d",p[l],at[l],bt[l],ct[l],tat[l],wt[l]);
 	 }
 	 
     
